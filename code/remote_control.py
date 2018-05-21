dc21304c01b9140e39cd8bf32898228761036595
Loosper <boian4o1@gmail.com>
2018-05-21 23:55:28 +0300

    Validate and send to socket in json

--- code/remote_control.py
+++ code/remote_control.py
@@ -1,10 +1,25 @@
 import serial
 import socket
+import logging
+import json
+from settings import RPI_ADDR, PORT, INT_SIZE
 
-from settings import RPI_ADDR, PORT
 
-
+logging.basicConfig(level='INFO')
 con = None
+soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+
+def check(con):
+    byte = con.read(1)
+    logging.debug('read: \\x{}'.format(byte.hex()))
+
+    if byte != b'\xFF':
+        logging.debug('byte failed')
+        return False
+
+    return True
+
 
 # arduino swaps between the two
 try:
@@ -13,14 +28,25 @@ except:
     raise
     con = serial.Serial('/dev/ttyUSB1', 9600)
 
-soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-soc.connect(RPI_ADDR, PORT)
+soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+address = (RPI_ADDR, PORT)
+
+# may be useless
+con.reset_input_buffer()
 
 while True:
-    try:
-        soc.send(con.read(1))
-    except:
-        raise
+    if not (check(con) and check(con)):
+        continue
 
-con.close()
+    msg = []
+
+    for _ in range(4):
+        logging.debug('reading shit')
+        msg.append(con.read(INT_SIZE))
+
+    parsed = [int.from_bytes(cord, byteorder='little') for cord in msg]
+    logging.info(parsed)
+
+    soc.sendto(json.dumps(parsed).encode('ascii'), (RPI_ADDR, PORT))
+
 con.close()
